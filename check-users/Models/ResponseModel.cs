979fc8a7c8e3438ba0fd3616137068d30eef5402
Cylliun <japaguilherme55@gmail.com>
2025-06-26 15:17:07 -0400

    feat: Padronização de resposta da API com ResponseModel

---
+++ check-users/Models/ResponseModel.cs
@@ -0,0 +1,9 @@
+﻿namespace check_users.Models
+{
+    public class ResponseModel<T>
+    {
+        public T? Dados { get; set; }
+        public bool Status { get; set; }
+        public string Message { get; set; }
+    }
+}
