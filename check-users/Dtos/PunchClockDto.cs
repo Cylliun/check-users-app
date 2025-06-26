b258fb42fc6b928808b89b8c072c0cc9f1680d7e
Cylliun <japaguilherme55@gmail.com>
2025-06-26 15:39:55 -0400

    feat: Uso de Dtos na lógica do sistema

---
+++ check-users/Dtos/PunchClockDto.cs
@@ -0,0 +1,15 @@
+﻿using check_users.Models;
+
+namespace check_users.Dtos
+{
+    public class PunchClockDto
+    {
+        public DateTime CheckInTime { get; set; }
+        public GeoLocation CheckInlocation { get; set; }
+
+        public DateTime? CheckOutTime { get; set; }
+        public GeoLocation? CheckOutlocation { get; set; }
+
+        public bool Status { get; set; }
+    }
+}
