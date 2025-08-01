6049d98713cdaff9b912b6dc054b99c82b7dc130
Cylliun <japaguilherme55@gmail.com>
2025-08-01 14:38:54 -0400

    refactor: Mudança do IPunchClockServices usando Dto e padronização de respostas com ResponseModel

---
+++ check-users/Services/PunchCLock/IPunchClockServices.cs
@@ -0,0 +1,13 @@
+﻿using check_users.Dtos;
+using check_users.Models;
+
+namespace check_users.Services.PunchCLock
+{
+    public interface IPunchClockServices
+    {
+        Task<ResponseModel<PunchClock>> CreateAsync(PunchClockDto punchClockDto);
+        Task<ResponseModel<PunchClock>> GetByUserIdAsync(int userId);
+        Task<ResponseModel<PunchClock>> UpdateCheckOutAsync(PunchClockDto punchClockDto);
+        Task<ResponseModel<PunchClock>> GetTodayPunchAsync(DateTime checkInTime);
+    }
+}
