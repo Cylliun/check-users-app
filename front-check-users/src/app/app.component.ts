016964e4e8ec9193c626f894e72f5fc3d6d2f2d0
Cylliun <japaguilherme55@gmail.com>
2025-08-25 10:34:32 -0400

    style: desenvolvendo a interface do login de funcionarios e RH

--- front-check-users/src/app/app.component.ts
+++ front-check-users/src/app/app.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HomeComponent } from './pages/home/home.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HomeComponent],
+  imports: [RouterOutlet],
   template: `
-    <app-home />
     <router-outlet />
   `,
   styles: [],
--- front-check-users/src/app/pages/login/login.component.ts
+++ front-check-users/src/app/pages/login/login.component.ts
@@ -5,9 +5,18 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   template: `
-    <p>
-      login works!
-    </p>
+  <div class="flex flex-col justify-center items-center p-6">
+    <div class="mb-4 p-4">
+      <h1 class="text-xl text-center text-text-primary font-bold">Login Checkin</h1>
+      <p class="text-center text-text-secondary font-normal">Sistema de controle de ponto</p>
+    </div>
+    <div class="flex flex-col gap-4 shadow-md rounded-lg p-4 bg-bg-primary">
+      <div class="p-2">
+        <h1 class="text-xl text-center text-text-primary font-bold">Acesso ao sistema</h1>
+        <p class="text-center text-text-secondary font-normal">Use seu email corporativo e senha</p>
+      </div>
+    </div>
+  </div>
   `,
   styles: ``
 })
